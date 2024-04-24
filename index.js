5086708afc13a5e6b4dc13734227cc18374dcfde
AsbjoernRT <asbjoernthomsen@gmail.com>
2024-04-24 16:35:26 +0200

    Live

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Ewrybodii')
+  res.send('Dette er gruppe 30s NutriTracker Projekt.')
 })
 
 app.listen(port, () => {
