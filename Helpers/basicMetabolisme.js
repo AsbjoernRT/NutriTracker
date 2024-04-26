82fcf38bbc99ecd8d1e7a699ca0fab1db9923cf1
hylle03 <mh@mhmedia.dk>
2024-04-26 16:50:05 +0200

    Opdatering

---
+++ Helpers/basicMetabolisme.js
@@ -0,0 +1,67 @@
+function calculateBasalMetabolism(age, gender, weight, height) {
+    let basalMetabolism = 0;
+
+    // Beregn basalstofskiftet baseret på alder, køn, vægt og højde
+    if (gender === 'male') {
+        if (age < 3) {
+            basalMetabolism = 0.249 * weight - 0.13;
+        } else if (age >= 4 && age <= 10) {
+            basalMetabolism = 0.095 * weight + 2.11;
+        } else if (age >= 11 && age <= 18) {
+            basalMetabolism = 0.074 * weight + 2.75;
+        } else if (age >= 19 && age <= 30) {
+            basalMetabolism = 0.064 * weight + 2.84;
+        } else if (age >= 31 && age <= 60) {
+            basalMetabolism = 0.0485 * weight + 3.67;
+        } else if (age >= 61 && age <= 75) {
+            basalMetabolism = 0.0499 * weight + 2.93;
+        } else if (age > 75) {
+            basalMetabolism = 0.035 * weight + 3.43;
+        }
+    } else if (gender === 'female') {
+        if (age < 3) {
+            basalMetabolism = 0.244 * weight - 0.13;
+        } else if (age >= 4 && age <= 10) {
+            basalMetabolism = 0.085 * weight + 2.03;
+        } else if (age >= 11 && age <= 18) {
+            basalMetabolism = 0.056 * weight + 2.9;
+        } else if (age >= 19 && age <= 30) {
+            basalMetabolism = 0.0615 * weight + 2.08;
+        } else if (age >= 31 && age <= 60) {
+            basalMetabolism = 0.0364 * weight + 3.47;
+        } else if (age >= 61 && age <= 75) {
+            basalMetabolism = 0.0386 * weight + 2.88;
+        } else if (age > 75) {
+            basalMetabolism = 0.041 * weight + 2.61;
+        }
+    }
+
+    basalMetabolism *= 239;
+
+    return basalMetabolism;
+}
+
+// Dette er et eksempel, vi skal kunne hente dette ned dynamisk og herefter tilføje Kcal til brugerens forbrænding.
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let age = 30;
+let gender = 'male';
+let weight = 75; // Brugerens vægt
+let height = 180; // Brugerens højde (kan være valgfrit, afhængigt af beregningsformlerne)
+
+// Beregn basal metabolisme
+let basalMetabolism = calculateBasalMetabolism(age, gender, weight, height);
+console.log("Basal metabolism:", basalMetabolism + " Kcal");
+
+// Hvis stofskifteData allerede er defineret, skal du opdatere det, ellers initialisere det som et tomt objekt
+let stofskifteData = JSON.parse(localStorage.getItem('stofskifteData')) || {};
+
+// Tilføj basal metabolisme til stofskifteData
+stofskifteData.basalMetabolism = basalMetabolism;
+
+// Gem opdaterede stofskifteData i lokal lagring
+localStorage.setItem('stofskifteData', JSON.stringify(stofskifteData));
+
+
+});
