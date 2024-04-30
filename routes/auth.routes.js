1bd1b01ac74461d03d582efc3c1a4329b8f6a7d3
AsbjoernRT <asbjoernthomsen@gmail.com>
2024-04-30 18:38:52 +0200

    asdfgdh

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,20 +6,20 @@ const router = express.Router();
 
 
 // Route for user registration
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    // Call the authService to register the user
-    await authService.registerUser(username, password);
-    // Log a message to the console when a user registers successfully
-    console.log(`User ${username} registered successfully`);
-    // Send a success response
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    // Handle errors
-    console.error('Error registering user:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+// router.post('/register', async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     // Call the authService to register the user
+//     await authService.registerUser(username, password);
+//     // Log a message to the console when a user registers successfully
+//     console.log(`User ${username} registered successfully`);
+//     // Send a success response
+//     res.status(201).json({ message: 'User registered successfully' });
+//   } catch (error) {
+//     // Handle errors
+//     console.error('Error registering user:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
 export default router;
